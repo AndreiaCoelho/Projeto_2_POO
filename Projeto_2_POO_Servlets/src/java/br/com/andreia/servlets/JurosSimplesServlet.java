3549d6d9014f4e8edc0d95085befd749bbe1993c
Jeferson <Jeferson@192.168.23.1>
2017-03-27 23:02:43 -0300

    Ajuste do Servlet Juros Composto

--- Projeto_2_POO_Servlets/src/java/br/com/andreia/servlets/JurosSimplesServlet.java
+++ Projeto_2_POO_Servlets/src/java/br/com/andreia/servlets/JurosSimplesServlet.java
@@ -37,7 +37,7 @@ public class JurosSimplesServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet: Juros Composto</title>");            
+            out.println("<title>Servlet: Juros Simples</title>");            
             out.println("</head>");
             out.println("<body>");
             double capitalInicial = 0;
